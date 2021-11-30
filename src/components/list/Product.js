24799dd79772a28db341af12969bca9729ba3755
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-12-01 00:17:32 +0530

    build the layout of product list

--- src/components/list/Product.js
+++ src/components/list/Product.js
@@ -1,7 +1,20 @@
 import classes from "./Product.module.css";
 
 const Product = ({ product }) => {
-  return <div></div>;
+  return (
+    <div className={classes.product}>
+      <img src={product.image} alt={product.title} />
+      <div className={classes.category}>{product.category}</div>
+
+      <div className={classes.info}>
+        <div className={classes.title}>{product.title}</div>
+
+        {/* <div className={classes.description}>{product.description}</div> */}
+
+        <div className={classes.price}>{product.price}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
